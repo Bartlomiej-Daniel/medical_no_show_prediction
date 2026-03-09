1f6750532d0e3f6b5d371633a5c6ec065cc50f0b
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-03-09 19:58:54 +0100

    Add: database schema

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE appointments (
+    appointment_id BIGINT,
+    gender TEXT,
+    scheduled_day TIMESTAMP,
+    appointment_day TIMESTAMP,
+    age INT,
+    neighbourhood TEXT,
+    scholarship INT,
+    hypertension INT,
+    diabetes INT,
+    alcoholism INT,
+    handicap INT,
+    sms_received INT,
+    no_show INT
+);
