412adb90477c5e57b3ade850185078adf76bace6
Bartłomiej Daniel <bartlomiej.patryk.daniel@gmail.com>
2026-03-10 16:38:57 +0100

    Add: RandomForest model training

---
+++ modeling/train_model.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from pathlib import Path
+
+
+data_dir = Path(__file__).resolve().parent.parent
+data_file = data_dir / "data" / "processed" 
+
+df = pd.read_csv(data_file / "features.csv")
+
+features = [
+    "age",
+    "waiting_days",
+    "sms_received",
+    "scholarship",
+    "hypertension",
+    "diabetes",
+    "alcoholism",
+    "handicap",
+    "appointment_weekday"
+]
+
+x = df[features]
+y = df["no_show"]
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, 
+    y, 
+    test_size=0.2, 
+    random_state=42
+    )
+
+x_test.to_csv(data_file / "x_test.csv", index=False)
+y_test.to_csv(data_file / "y_test.csv", index=False)
+
+model = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+    )
+
+model.fit(x_train, y_train)
+
+model_dir = Path(__file__).parent
+joblib.dump(model, model_dir / "model.pkl")
+
+print("Model trained and saved")
+
